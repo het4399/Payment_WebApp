541b6eb0df8462528ea7e9c08133ccf8ae9f2bc4
het4399 <het.sojitra4399@gmail.com>
2025-04-19 10:03:54 +0530

    Added Appbar Component and issue of tailwindcss while importing from packages/ui

--- apps/UserApp/app/page.tsx
+++ apps/UserApp/app/page.tsx
@@ -1,10 +1,14 @@
+"use client"
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { Appbar } from "@repo/ui/Appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+
 export default function Home() {
+  const session = useSession();
   return (
     <div className="text-5xl">
-      Hello There
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
     </div>
   );
 }
